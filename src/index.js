80f9a5b170498b75d2bc19337c9c5d5026d922d3
John Ludena <johnludena@gmail.com>
2018-05-15 07:17:59 -0500

    style updates, time stamp added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/skeleton-css/css/skeleton.css';
+// import '../node_modules/skeleton-css/css/skeleton.css';
+import pizzaAnimation from './images/pizza-3-final.gif';
 // import botAvatar from './images/bot-avatar.png';
 
 class App extends React.Component {
@@ -12,9 +13,11 @@ class App extends React.Component {
         this.state = {
             userMessages: [],
             botMessages: [],
+            botGreeting: 'Hi there! My name is PizzaBot. I can help you get pizza! You can say something like \'I want a pepperoni pizza delivered to 12345 Main Ave.\' Try it below!',
             botLoading: false,
         }
     }
+
 
     updateUserMessages = (newMessage) => {
 
@@ -33,7 +36,7 @@ class App extends React.Component {
         // Get the request to DialogFlow in a nice little package with the user's message
         var request = new Request('https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=' + newMessage + '&sessionId=12345', {
             headers: new Headers({
-                "Authorization": "Bearer a38639f6a5a94fbbb1ef697e95a3d615"
+                "Authorization": "Bearer 4aae918f5eaf4ed4af292471a2852af4"
             })
         });
 
@@ -42,6 +45,12 @@ class App extends React.Component {
         .then(response => response.json())
         .then(json => {
             console.log('BOT RESPONSE:', json.result.fulfillment.speech);
+
+            // End conversation once user hits end flag in API
+            var endConvoFlag = json.result.metadata.endConversation;
+            if (endConvoFlag !== undefined || endConvoFlag === true) {
+                console.log('end of conversation. order placed.')
+            }
 
             var botResponse = json.result.fulfillment.speech;
 
@@ -62,11 +71,11 @@ class App extends React.Component {
 
         var userConvo = this.state.userMessages;
 
-        // Exit function and stop rendering if user hasnt' started conversation yet
+        // Show initial bot welcome message
         if (this.state.userMessages.length === 0) {
-            return;
-        } 
-
+            return 
+        }
+        
         var updatedConvo = userConvo.map((data, index)=>{
 
             var botResponse = this.state.botMessages[index];
@@ -87,7 +96,11 @@ class App extends React.Component {
 
         return (
             <div id="app-container">
-                <div className="convo-container">{this.showMessages()}</div>
+               
+                <div className="convo-container">
+                    <BotBubble message={this.state.botGreeting} key="bot-00" />
+                    {this.showMessages()}
+                </div>
                 <UserInput userMessage = {this.state.userMessage} updateUserMessages = {this.updateUserMessages} />
             </div>
             
@@ -116,6 +129,15 @@ class BotBubble extends React.Component {
         lastBubble.scrollIntoView(true);
     }
 
+    getTimeStamp = () => {
+        var time = new Date(); // create a new Date object
+        
+        // Format date in 12 hour format (AM/PM) using 'toLocaleString'
+        var formattedDate = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+        return formattedDate;
+    }
+
     render() {
 
         return (
@@ -123,9 +145,12 @@ class BotBubble extends React.Component {
                 <div className="img-avatar-container">
                     <img className="bot-avatar" src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="bot avatar" />
                 </div>
-                
-                <div className="chat-bubble bot" ref="chatBubble">{this.props.message ? this.props.message : '...'}</div>
+                <div>
+                    <div className="chat-bubble bot" ref="chatBubble">{this.props.message ? this.props.message : '...'}</div>
+                    <div className="time-stamp">{this.getTimeStamp()}</div>
+                </div>
             </div>
+            
         )
     }
 }
